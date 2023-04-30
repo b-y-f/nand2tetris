c0cee51c1c9e9cd2be9cf0ddd5e59ef9e8ec5ca0
b-y-f <bbbbb8bbbbb@gmail.com>
2023-04-30 22:34:07 +1200

    add: proj6 - add helper function and other

--- projects/06/hack_assembler.py
+++ projects/06/hack_assembler.py
@@ -1,22 +1,61 @@
 # Use: python app.py xxx.asm -> new file created includes 16bit binary code
 
 import re
+import sys
+import symbol_table as symbols
 
-acommands=[]
-asm=[]
+def load_asm_and_clean():
+    with open(sys.argv[1], 'r') as file:
+        lines = file.readlines()
 
-# https://people.duke.edu/~nts9/asmtoml/assembler.py
-dest={'':'000','M=':'001','D=':'010','MD=':'011',
-      'A=':'100','AM=':'101','AD=':'110','AMD=':'111'}
-jump={'':'000',';JGT':'001',';JEQ':'010',';JGE':'011',
-      ';JLT':'100',';JNE':'101',';JLE':'110',';JMP':'111'}
-comp={'0':'0101010','1':'0111111','-1':'0111010','D':'0001100',
-      'A':'0110000','M':'1110000','!D':'0001101','!A':'0110001',
-      '!M':'1110001','-D':'0001111','-A':'0110011','-M':'1110011',
-      'D+1':'0011111','A+1':'0110111','M+1':'1110111','D-1':'0001110',
-      'A-1':'0110010','M-1':'1110010','D+A':'0000010','D+M':'1000010',
-      'D-A':'0010011','D-M':'1010011','A-D':'0000111','M-D':'1000111',
-      'D&A':'00000000','D&M':'1000000','D|A':'0010101','D|M':'1010101'}
-symbols={'SP':0,'LCL':1,'ARG':2,'THIS':3,'THAT':4,'SCREEN':16384,'KBD':24576,
-         'R0':0,'R1':1,'R1':1,'R2':2,'R3':3,'R4':4,'R5':5,'R6':6,'R7':7,
-         'R8':8,'R9':9,'R10':10,'R11':11,'R12':12,'R13':13,'R14':14,'R15':15}
+    pure_code_lines = []
+    for line in lines:
+        if line != '\n':
+            comment_index = line.find("//")
+            if comment_index == -1:
+                pure_code_lines.append(line.strip())
+            elif comment_index > 0:
+                pure_code_lines.append(line[:comment_index].strip())
+
+    # print(pure_code_lines)
+    return pure_code_lines
+
+def instruction_type():
+    """Returns the current instruction type (constant):
+    A_INSTRUCTION for @ xxx, where xxx is either a decimal number or a symbol 
+    C_INSTRUCTION for dest = comp ; jump
+    L_INSTRUCTION for (label)
+    """
+    pass
+
+def symbol(): 
+    # Returns the instruction’s symbol (string) 
+    pass
+def dest(): 
+    # Returns the instruction’s dest field (string) 
+    pass
+    
+def comp(): 
+    # Returns the instruction’s comp field (string) 
+    pass
+def jump(): 
+    # Returns the instruction’s jump field (string)
+    pass
+
+def parse_line(line):
+    if line in symbols:
+        pass
+
+    first_char = line[0]
+    if first_char == '@':
+        # tranlate to 16 bit
+        bin_ = bin(int(line[1:]))[2:].zfill(16)
+        print(bin_)
+        return bin_
+        
+lines = load_asm_and_clean()
+
+for l in lines:
+    parse_line(l)
+    break
+
